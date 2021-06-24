01968b3ba2f2a0b7ddf475c8010e6d81078294d1
Dilenio Enderle <dilenio@gmail.com>
2021-06-24 06:07:41 -0300

    Last commit at Trybe

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,33 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if not instance.find_file(path_file):
+        content = txt_importer(path_file)
+        instance.enqueue(path_file)
+        lines = list()
+        for line in content:
+            lines.append(line)
+        lines_count = len(lines)
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": lines_count,
+            "linhas_do_arquivo": content
+        }
+
+        sys.stdout.write(str(result))
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if instance.__len__() == 0:
+        return sys.stdout.write("Não há elementos\n")
+    file = instance.dequeue()
+    sys.stdout.write(
+        f"Arquivo {file} removido com sucesso\n"
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    if position > instance.__len__() - 1:
+        return sys.stderr.write("Posição inválida\n")
--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,42 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    data_content = instance.return_data()
+    result = list()
+    for file in data_content:
+        file_content = txt_importer(file)
+        count = list()
+        for index, line in enumerate(file_content):
+            if word.casefold() in line.casefold():
+                count.append({"linha": index + 1})
+        if len(count) != 0:
+            occur_result = {
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": count
+            }
+            result.append(occur_result)
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    data_content = instance.return_data()
+    result = list()
+    for file in data_content:
+        file_content = txt_importer(file)
+        count = list()
+        for index, line in enumerate(file_content):
+            if word.casefold() in line.casefold():
+                count.append({
+                    "linha": index + 1,
+                    "conteudo": line
+                })
+        if len(count) != 0:
+            occur_result = {
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": count
+            }
+            result.append(occur_result)
+    return result
